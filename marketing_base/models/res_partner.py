0633a31320686ed51f8aeb805821730ad25af28b
Xavier Bouquiaux <xavier.bouquiaux@gmail.com>
2022-01-04 21:56:21 +0100

    marketing_base
    res_partner constraint

--- marketing_base/models/res_partner.py
+++ marketing_base/models/res_partner.py
@@ -1,8 +1,20 @@
 # Copyright 2022 Xavier Bouquiaux
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
+
+    """create a constraint to restrict 2 active partner with the same email"""
+
+    @api.constrains("email")
+    def _check_email_unicity(self):
+        for partner in self:
+            emails = self.env["res.partner"].search(
+                [("email", "=", partner.email), ("id", "!=", partner.id)]
+            )
+            if emails:
+                raise ValidationError(_("Another partner exists with the same email"))
